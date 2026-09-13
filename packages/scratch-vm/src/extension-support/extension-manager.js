0326061b1a47a6adf0eef1c7570d6dca4a994951
svl <pacific.hat.vr6755@gmail.com>
2026-09-13 16:58:10 -0400

    Add localauth extension to extension manager

--- packages/scratch-vm/src/extension-support/extension-manager.js
+++ packages/scratch-vm/src/extension-support/extension-manager.js
@@ -24,7 +24,8 @@ const builtinExtensions = {
     makeymakey: () => require('../extensions/scratch3_makeymakey'),
     boost: () => require('../extensions/scratch3_boost'),
     gdxfor: () => require('../extensions/scratch3_gdx_for'),
-    faceSensing: () => require('../extensions/scratch3_face_sensing')
+    faceSensing: () => require('../extensions/scratch3_face_sensing'),
+    localauth: () => require('../extensions/opusmod_localauth')
 };
 
 /**
